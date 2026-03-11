008ce11098655cd0970c8c50fb61c8f425df8d25
Ari586 <la7ariel@gmail.com>
2026-03-11 05:04:03 +0100

    Update web deployment: admin recap IA

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e8862ea0c44e4425763300e1a58b635e",
+const RESOURCES = {"flutter_bootstrap.js": "8219d410cc4cf427ef9d111ac7daa59a",
 "version.json": "c4799dcdc2bcb8a150de7013cea913fa",
 "index.html": "3dad928e9876843292987739aaea36e8",
 "/": "3dad928e9876843292987739aaea36e8",
-"main.dart.js": "4e0997975a9a16dba53fe5d86bc73a29",
+"main.dart.js": "af2ef9529cbe62a28c4ecc39d2c5f51b",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
